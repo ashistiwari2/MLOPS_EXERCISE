bdbbf6b7e11c9d882b4c84f9466ba01bdde64df3
ashistiwari2 <66716885+ashistiwari2@users.noreply.github.com>
2022-08-30 13:07:08 +0530

    Update database.py

--- Logistic_api/database.py
+++ Logistic_api/database.py
@@ -10,9 +10,9 @@ db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '5
 database_name = os.environ.get('database_name', 'fastapi')
 db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'postgres')))
 db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'secret')))
-ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','prefer')))
-SQLALCHEMY_DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port, database_name, ssl_mode)
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+#ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','prefer')))
+#SQLALCHEMY_DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port, database_name, ssl_mode)
+SQLALCHEMY_DATABASE_URL = "postgresql://{}:{}@{}/{}".format(db_username, db_password, host_server,database_name)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, pool_size=3, max_overflow=0
